07e6b3bfe9b414adf18c7ed5c320d8d43f021d91
antonszubkovs <antons.zubkovs@gmail.com>
2022-08-17 16:51:09 +0300

    MOBILE-1251_part4: Initial kevin core plugin iOS native part

---
+++ packages/kevin_flutter_core/ios/Classes/KevinMethodArguments.swift
@@ -0,0 +1,5 @@
+internal struct KevinMethodArguments {
+    static let languageCode = "languageCode"
+    static let sandbox = "sandbox"
+    static let deepLinkingEnabled = "deepLinkingEnabled"
+}
--- packages/kevin_flutter_core/ios/Classes/SwiftKevinFlutterCorePlugin.swift
+++ packages/kevin_flutter_core/ios/Classes/SwiftKevinFlutterCorePlugin.swift
@@ -2,13 +2,37 @@ import Flutter
 import UIKit
 
 public class SwiftKevinFlutterCorePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "kevin_flutter_core", binaryMessenger: registrar.messenger())
-    let instance = SwiftKevinFlutterCorePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "kevin_flutter_core", binaryMessenger: registrar.messenger())
+        let instance = SwiftKevinFlutterCorePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        result("iOS " + UIDevice.current.systemVersion)
+    }
+    
+    private func onSetLocale(){
+        
+    }
+    
+    private func onSetTheme(){
+        
+    }
+    
+    private func onSetDeepLinkingEnabled(){
+        
+    }
+    
+    private func onGetLocale(){
+        
+    }
+    
+    private func onIsSandbox(){
+        
+    }
+    
+    private func onIsDeepLinkingEnabled(){
+        
+    }
 }
