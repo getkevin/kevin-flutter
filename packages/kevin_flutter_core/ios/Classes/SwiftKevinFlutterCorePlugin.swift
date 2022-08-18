431c5bdac3f4b2350942d1aa86b1d8c5515a8fcc
antonszubkovs <antons.zubkovs@gmail.com>
2022-08-18 14:17:15 +0300

    MOBILE-1251_part4: Add kevin core plugin iOS implementation

--- packages/kevin_flutter_core/ios/Classes/SwiftKevinFlutterCorePlugin.swift
+++ packages/kevin_flutter_core/ios/Classes/SwiftKevinFlutterCorePlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import kevin_ios
 
 public class SwiftKevinFlutterCorePlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -9,30 +10,70 @@ public class SwiftKevinFlutterCorePlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result("iOS " + UIDevice.current.systemVersion)
+        switch KevinMethod(rawValue: call.method) {
+        case .setLocale:
+            onSetLocale(call: call, result: result)
+        case .setTheme:
+            onSetTheme()
+        case .setSandbox:
+            onSetSandbox(call: call, result: result)
+        case .setDeepLinkingEnabled:
+            onSetDeepLinkingEnabled(call: call, result: result)
+        case .getLocale:
+            onGetLocale(result: result)
+        case .isSandbox:
+            onIsSandbox(result: result)
+        case .isDeepLinkingEnabled:
+            onIsDeepLinkingEnabled(result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
     
-    private func onSetLocale(){
+    private func onSetLocale(call: FlutterMethodCall, result: @escaping FlutterResult){
+        let langArg = (call.arguments as? [String: String])?[KevinMethodArguments.languageCode]
         
+        if let lang = langArg {
+            let locale = Locale(identifier: lang)
+            Kevin.shared.locale = locale
+        }
+        
+        result(nil)
     }
     
     private func onSetTheme(){
         
     }
     
-    private func onSetDeepLinkingEnabled(){
+    private func onSetSandbox(call: FlutterMethodCall, result: @escaping FlutterResult){
+        let isSandboxArg = (call.arguments as? [String: Bool])?[KevinMethodArguments.sandbox]
         
+        if let isSandbox = isSandboxArg {
+            Kevin.shared.isSandbox = isSandbox
+        }
+        
+        result(nil)
     }
     
-    private func onGetLocale(){
+    private func onSetDeepLinkingEnabled(call: FlutterMethodCall, result: @escaping FlutterResult){
+        let isDeepLinkingEnabledArg = (call.arguments as? [String: Bool])?[KevinMethodArguments.deepLinkingEnabled]
         
+        if let isDeepLinkingEnabled = isDeepLinkingEnabledArg {
+            Kevin.shared.isDeepLinkingEnabled = isDeepLinkingEnabled
+        }
+        
+        result(nil)
     }
     
-    private func onIsSandbox(){
-        
+    private func onGetLocale(result: @escaping FlutterResult){
+        result(Kevin.shared.locale.identifier)
     }
     
-    private func onIsDeepLinkingEnabled(){
-        
+    private func onIsSandbox(result: @escaping FlutterResult){
+        result(Kevin.shared.isSandbox)
+    }
+    
+    private func onIsDeepLinkingEnabled(result: @escaping FlutterResult){
+        result(Kevin.shared.isDeepLinkingEnabled)
     }
 }
