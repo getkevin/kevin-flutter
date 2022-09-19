03d7d87d6a786ab87b6fc5dc18994c0fabfa00bd
antonszubkovs <antons.zubkovs@gmail.com>
2022-09-19 11:45:44 +0300

    MOBILE-1251_part11: Add explanatory comments to theme classes

--- packages/kevin_flutter_core/kevin_flutter_core_platform_interface/lib/src/model/theme/android/kevin_theme_android.dart
+++ packages/kevin_flutter_core/kevin_flutter_core_platform_interface/lib/src/model/theme/android/kevin_theme_android.dart
@@ -1,3 +1,8 @@
+/// Android theme's resource name.
+///
+/// Android theme resource file must be created inside application's
+/// android resources folder, e.x., your_app/android/app/src/main/res/values/themes.xml.
+/// More info: https://app.gitbook.com/o/408aMueAbgo10HRzg6FC/s/7XHN8bnfHxbgltrB2j38/~/changes/4OTuNAVn9kGqufNWHObU/flutter/ui-customisation/android-ui-customisations
 class KevinThemeAndroid {
   final String themeName;
 
