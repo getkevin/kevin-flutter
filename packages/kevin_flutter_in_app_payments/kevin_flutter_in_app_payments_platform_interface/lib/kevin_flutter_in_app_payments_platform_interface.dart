f9c69a1957d3bca366a6db1a5c5cb34759de4449
antonszubkovs <antons.zubkovs@gmail.com>
2022-10-06 10:47:16 +0300

    MOBILE-1264_part11: Add omitted export in kevin_flutter_in_app_payments_platform_interface.dart

--- packages/kevin_flutter_in_app_payments/kevin_flutter_in_app_payments_platform_interface/lib/kevin_flutter_in_app_payments_platform_interface.dart
+++ packages/kevin_flutter_in_app_payments/kevin_flutter_in_app_payments_platform_interface/lib/kevin_flutter_in_app_payments_platform_interface.dart
@@ -1,4 +1,5 @@
 library kevin_flutter_in_app_payments_platform_interface;
 
 export 'src/kevin_flutter_payments_platform_interface.dart';
+export 'src/model/kevin_flutter_payments_methods.dart';
 export 'src/model/kevin_payments_models.dart';
