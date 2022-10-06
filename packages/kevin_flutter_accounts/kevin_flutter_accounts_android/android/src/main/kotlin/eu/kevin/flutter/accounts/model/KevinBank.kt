08a30d157155ba9ed1df076e3c18302294e0cbdf
antonszubkovs <antons.zubkovs@gmail.com>
2022-10-06 11:20:00 +0300

    MOBILE-1264_part12: Introduce kevin_flutter_accounts_android

---
+++ packages/kevin_flutter_accounts/kevin_flutter_accounts_android/android/src/main/kotlin/eu/kevin/flutter/accounts/model/KevinBank.kt
@@ -0,0 +1,15 @@
+package eu.kevin.flutter.accounts.model
+
+import eu.kevin.accounts.bankselection.entities.Bank
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class KevinBank(
+    val id: String,
+    val name: String,
+    val officialName: String? = null,
+    val imageUri: String,
+    val bic: String? = null,
+)
+
+internal fun Bank.toKevinBank() = KevinBank(id, name, officialName, imageUri, bic)
---
+++ packages/kevin_flutter_accounts/kevin_flutter_accounts_android/test/kevin_flutter_accounts_android_test.dart
@@ -0,0 +1,256 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kevin_flutter_accounts_android/kevin_flutter_accounts_android.dart';
+import 'package:kevin_flutter_accounts_platform_interface/kevin_flutter_accounts_platform_interface.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const channel = MethodChannel('kevin_flutter_accounts_android');
+  final log = <MethodCall>[];
+
+  final platform = KevinFlutterAccountsAndroid();
+
+  void _setMethodCallReturnData({dynamic Function()? data}) {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      log.add(methodCall);
+      return data?.call();
+    });
+  }
+
+  setUp(() {
+    _setMethodCallReturnData();
+  });
+
+  tearDown(() {
+    log.clear();
+  });
+
+  test('registers instance', () {
+    KevinFlutterAccountsAndroid.registerWith();
+    expect(
+      KevinFlutterAccountsPlatformInterface.instance,
+      isA<KevinFlutterAccountsAndroid>(),
+    );
+  });
+
+  test('setAccountsConfiguration', () async {
+    const configuration = KevinAccountsConfiguration(
+      callbackUrl: KevinCallbackUrl(android: 'android', ios: 'ios'),
+      showUnsupportedBanks: true,
+    );
+    await platform.setAccountsConfiguration(configuration);
+    expect(log, <Matcher>[
+      isMethodCall(
+        'setAccountsConfiguration',
+        arguments: <String, dynamic>{
+          'callbackUrl': 'android',
+          'showUnsupportedBanks': true
+        },
+      )
+    ]);
+  });
+
+  test('startAccountLinking: success', () async {
+    const successResult = KevinSessionResultLinkingSuccess(
+      bank: KevinBank(
+        id: 'id',
+        name: 'name',
+        officialName: 'officialName',
+        imageUri: 'imageUri',
+        bic: 'bic',
+      ),
+      authorizationCode: 'authorizationCode',
+      linkingType: KevinAccountLinkingType.bank,
+    );
+
+    final mockedResult = jsonEncode(successResult.toMap());
+
+    _setMethodCallReturnData(data: () => mockedResult);
+
+    const configuration = KevinAccountSessionConfiguration(state: 'state');
+    final result = await platform.startAccountLinking(configuration);
+    expect(result, isInstanceOf<KevinSessionResultLinkingSuccess>());
+    expect(log, <Matcher>[
+      isMethodCall(
+        'startAccountLinking',
+        arguments: <String, dynamic>{
+          'state': 'state',
+          'preselectedCountry': null,
+          'disableCountrySelection': false,
+          'countryFilter': [],
+          'bankFilter': [],
+          'preselectedBank': null,
+          'skipBankSelection': false,
+          'accountLinkingType': 'bank',
+        },
+      )
+    ]);
+  });
+
+  test('startAccountLinking: cancelled', () async {
+    _setMethodCallReturnData(
+      data: () {
+        throw PlatformException(code: 'cancelled');
+      },
+    );
+
+    const configuration = KevinAccountSessionConfiguration(state: 'state');
+    final result = await platform.startAccountLinking(configuration);
+    expect(result, isInstanceOf<KevinSessionResultCancelled>());
+    expect(log, <Matcher>[
+      isMethodCall(
+        'startAccountLinking',
+        arguments: <String, dynamic>{
+          'state': 'state',
+          'preselectedCountry': null,
+          'disableCountrySelection': false,
+          'countryFilter': [],
+          'bankFilter': [],
+          'preselectedBank': null,
+          'skipBankSelection': false,
+          'accountLinkingType': 'bank',
+        },
+      )
+    ]);
+  });
+
+  test('startAccountLinking: general', () async {
+    _setMethodCallReturnData(
+      data: () {
+        throw PlatformException(code: 'general');
+      },
+    );
+
+    const configuration = KevinAccountSessionConfiguration(state: 'state');
+    final result = await platform.startAccountLinking(configuration);
+    expect(result, isInstanceOf<KevinSessionResultGeneralError>());
+    expect(log, <Matcher>[
+      isMethodCall(
+        'startAccountLinking',
+        arguments: <String, dynamic>{
+          'state': 'state',
+          'preselectedCountry': null,
+          'disableCountrySelection': false,
+          'countryFilter': [],
+          'bankFilter': [],
+          'preselectedBank': null,
+          'skipBankSelection': false,
+          'accountLinkingType': 'bank',
+        },
+      )
+    ]);
+  });
+
+  test('startAccountLinking: unexpected', () async {
+    _setMethodCallReturnData(
+      data: () {
+        throw PlatformException(code: 'unexpected');
+      },
+    );
+
+    const configuration = KevinAccountSessionConfiguration(state: 'state');
+    expect(
+      () async {
+        await platform.startAccountLinking(configuration);
+      },
+      throwsA(isA<PlatformException>()),
+    );
+    expect(log, <Matcher>[
+      isMethodCall(
+        'startAccountLinking',
+        arguments: <String, dynamic>{
+          'state': 'state',
+          'preselectedCountry': null,
+          'disableCountrySelection': false,
+          'countryFilter': [],
+          'bankFilter': [],
+          'preselectedBank': null,
+          'skipBankSelection': false,
+          'accountLinkingType': 'bank',
+        },
+      )
+    ]);
+  });
+
+  test('startAccountLinking: null', () async {
+    const configuration = KevinAccountSessionConfiguration(state: 'state');
+    expect(
+      () async {
+        await platform.startAccountLinking(configuration);
+      },
+      throwsA(isA<TypeError>()),
+    );
+    expect(log, <Matcher>[
+      isMethodCall(
+        'startAccountLinking',
+        arguments: <String, dynamic>{
+          'state': 'state',
+          'preselectedCountry': null,
+          'disableCountrySelection': false,
+          'countryFilter': [],
+          'bankFilter': [],
+          'preselectedBank': null,
+          'skipBankSelection': false,
+          'accountLinkingType': 'bank',
+        },
+      )
+    ]);
+  });
+
+  test('getCallbackUrl: has value', () async {
+    _setMethodCallReturnData(data: () => 'callbackUrl');
+    final callbackUrl = await platform.getCallbackUrl();
+    expect(callbackUrl, 'callbackUrl');
+    expect(log, <Matcher>[
+      isMethodCall(
+        'getCallbackUrl',
+        arguments: null,
+      )
+    ]);
+  });
+
+  test('getCallbackUrl: has no value', () async {
+    expect(
+      () async {
+        await platform.getCallbackUrl();
+      },
+      throwsA(isA<TypeError>()),
+    );
+    expect(log, <Matcher>[
+      isMethodCall(
+        'getCallbackUrl',
+        arguments: null,
+      )
+    ]);
+  });
+
+  test('isShowUnsupportedBanks: has value', () async {
+    _setMethodCallReturnData(data: () => true);
+    final callbackUrl = await platform.isShowUnsupportedBanks();
+    expect(callbackUrl, true);
+    expect(log, <Matcher>[
+      isMethodCall(
+        'isShowUnsupportedBanks',
+        arguments: null,
+      )
+    ]);
+  });
+
+  test('isShowUnsupportedBanks: has no value', () async {
+    expect(
+      () async {
+        await platform.isShowUnsupportedBanks();
+      },
+      throwsA(isA<TypeError>()),
+    );
+    expect(log, <Matcher>[
+      isMethodCall(
+        'isShowUnsupportedBanks',
+        arguments: null,
+      )
+    ]);
+  });
+}
